69d8f8ae625a1979600df1797acd209d6cd88f92
mimilettd <mimilettd@gmail.com>
2017-10-30 21:29:48 -0600

    write test: user can create account without oauth

---
+++ spec/features/user_can_create_new_account_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+feature "User can create an account" do
+  scenario "without Oauth" do
+    visit '/'
+
+    click_on "Sign In"
+
+    expect(current_path).to eq('/signin')
+
+    click_on "Create your ReBook account"
+
+    expect(current_path).to eq('/newaccount')
+
+    fill_in "Your name", with: "Mimi Le"
+    fill_in "Email", with: "mimi@rebook.com"
+    fill_in "Password", with: "password"
+    fill_in "Re-enter password", with: "password"
+
+    click_on "Create your ReBook account"
+
+    expect(current_path).to eq('/myaccount')
+  end
+end
