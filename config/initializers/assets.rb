82df6952d269fc793470495494ce53a1bd1fef7d
mimilettd <mimilettd@gmail.com>
2017-11-03 12:49:24 -0600

    implement star rating system

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,6 +15,7 @@ Rails.application.config.assets.precompile += %w( navigation.css )
 Rails.application.config.assets.precompile += %w( root.css )
 Rails.application.config.assets.precompile += %w( search_bar.css )
 Rails.application.config.assets.precompile += %w( fonts.css )
+Rails.application.config.assets.precompile += %w( stars.css )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
