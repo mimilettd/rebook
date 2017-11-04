ebe990ebad73f14c6323a11c7e44fd3bd37a87bb
mimilettd <mimilettd@gmail.com>
2017-11-04 14:47:25 -0600

    style book description

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  helper_method :current_user
+  helper_method :current_user, :categories
 
   def current_user
     @user = User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def categories
+    @categories = Category.all.order(:name)
   end
 end
