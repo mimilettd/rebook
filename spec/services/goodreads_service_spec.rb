5ae28d29651344bc548c7368216fca56be426849
mimilettd <mimilettd@gmail.com>
2017-11-04 14:56:33 -0600

    refactor test with real data

--- spec/services/goodreads_service_spec.rb
+++ spec/services/goodreads_service_spec.rb
@@ -4,12 +4,12 @@ describe "GoodreadsService" do
   it ".fetch_reviews(isbn)" do
     raw_data = GoodreadsService.fetch_reviews("9781419725456")
 
-    expect(raw_data.count).to eq(1)
+    expect(raw_data).to be_a(String)
   end
 
   it ".fetch_average_rating(isbn)" do
     raw_data = GoodreadsService.fetch_average_rating("9781419725456")
 
-    expect(raw_data.count).to eq(1)
+    expect(raw_data).to be_a(String)
   end
 end
