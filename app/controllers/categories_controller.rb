9ab0ded728de2274300b3493a11e023a24ef177a
mimilettd <mimilettd@gmail.com>
2017-11-04 20:27:09 -0600

    create a categories#index page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,10 +1,13 @@
 class CategoriesController < ApplicationController
   def index
-
+    @categories = Category.all.order(:name)
   end
 
   def show
-    @category = Category.find_by(slug: params["slug"])
+    @category = Category.find_by(slug: params["slug"] ||= params["id"])
+    if @category.nil?
+      @category = Category.find(params["id"])
+    end
     @books = @category.books
   end
 end
