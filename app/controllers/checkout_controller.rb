d4a123ea4c98118a3a08a5a8a1078862875cd4fc
mimilettd <mimilettd@gmail.com>
2017-11-26 17:00:42 -0700

    user must update their address before they can checkout

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -4,12 +4,17 @@ class CheckoutController < ApplicationController
   end
 
   def create
-    book = Book.find_by(slug: params[:book])
-    loan = Loan.new(user_id: current_user.id, book_id: book.id)
-    if loan.save
-      redirect_to myaccount_path
+    if current_user.street_address.nil?
+      flash[:notice] = "Please update your address to checkout."
+      redirect_to account_settings_path
     else
-      redirect_to root_path
+      book = Book.find_by(slug: params[:book])
+      loan = Loan.new(user_id: current_user.id, book_id: book.id)
+      if loan.save
+        redirect_to myaccount_path
+      else
+        redirect_to root_path
+      end
     end
   end
 end
